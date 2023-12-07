5bbb675ccca3612097db29e1fc1b4c3a7733be38
Shcherbanev Konstantin <shcherbanev1@yandex.ru>
2023-12-07 21:50:05 +0300

    fill funcs for Subs and Users fixed + their realisation checked

--- classwork/20231207/Main.java
+++ classwork/20231207/Main.java
@@ -1,25 +1,69 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 	
 	public static void main(String [] args) {
-		File file = new File("Users.txt");
-		fillUser(file);
-	}
-	
-	public static void fillUser(File file) {
+		
+		User [] users = null;
+		Subscription [] subs = null;
+		
 		try {
-			Scanner sc = new Scanner(file);
-			int n = sc.nextInt();
-			sc.nextLine();
-			for (int i = 0; i < n; i++) {
-				String str = sc.nextLine();
-				System.out.println(str);
-			}
+			File usersFile = new File("Users.txt");
+			users = fillUser(usersFile);
 		} catch (Exception e) {
 			System.out.println("error");
 		}
+		
+		try {
+			File subsFile = new File("Subscriptions.txt");
+			subs = fillSubs(subsFile);
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+		
+		if (users != null && subs != null) {
+
+			for (int i = 0; i < users.length; i++) {
+				System.out.println(users[i]);
+			}
+			for (int i = 0; i < subs.length; i++) {
+				System.out.println(subs[i]);
+			}
+			
+		}
+	}
+	
+	public static User[] fillUser(File file) throws FileNotFoundException {
+		Scanner sc = new Scanner(file);
+		int n = sc.nextInt();
+		User[] users = new User[n];
+		sc.nextLine();
+		for (int i = 0; i < n; i++) {
+			String[] str = sc.nextLine().split(" ");
+			int id = Integer.parseInt(str[0]);
+			String name = str[1];
+			String city = str[2];
+			User user = new User(id, name, city);
+			users[i] = user;
+		}
+		return users;
+	}
+	
+	public static Subscription[] fillSubs(File file) throws FileNotFoundException {
+		Scanner sc = new Scanner(file);
+		int n = sc.nextInt();
+		Subscription[] subs = new Subscription[n];
+		sc.nextLine();
+		for (int i = 0; i < n; i++) {
+			String[] str = sc.nextLine().split(" ");
+			int user1_id = Integer.parseInt(str[0]);
+			int user2_id = Integer.parseInt(str[1]);
+			Subscription sub = new Subscription(user1_id, user2_id);
+			subs[i] = sub;
+		}
+		return subs;
 	}
 	
 }
--- classwork/20231207/Subscription.java
+++ classwork/20231207/Subscription.java
@@ -1,6 +1,23 @@
 public class Subscription {
 	
-	private user1_id;
-	private user2_id;
+	private int user1_id;
+	private int user2_id;
+	
+	public Subscription(int user1_id, int user2_id) {
+		this.user1_id = user1_id;
+		this.user2_id = user2_id;
+	}
+
+	public int getFirstId() {
+		return user1_id;
+	}
+	
+	public int getSecondId() {
+		return user2_id;
+	}
+	
+	public String toString() {
+		return user1_id + " " + user2_id;
+	}
 	
 }
