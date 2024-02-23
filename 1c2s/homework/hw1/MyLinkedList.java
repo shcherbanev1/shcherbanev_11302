bef520e647c313b556b6b7ed6ca175c7a1cd61b5
Shcherbanev Konstantin <shcherbanev1@yandex.ru>
2024-02-23 19:43:14 +0300

    hw1 done

---
+++ 1c2s/homework/hw1/MyLinkedList.java
@@ -0,0 +1,269 @@
+import java.util.Scanner;
+
+public class MyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size = 0;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = head;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void inputList(int n) {
+        for (int i = 0; i < n; i++) {
+            Scanner sc = new Scanner(System.in);
+            this.add(sc.nextInt());
+        }
+    }
+
+    public int max() {
+        Node temp = head;
+        int max = head.getData();
+        while (temp != null) {
+            temp = temp.getNext();
+            if (temp.getData() > max) {
+                max = temp.getData();
+            }
+        }
+        return max;
+    }
+
+    public boolean contains(int n) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.getData() == n) {
+                return true;
+            }
+            temp = temp.getNext();
+        }
+        return false;
+    }
+
+    private static void swap(Node node1, Node node2) {
+        int temp = node2.getData();
+        node2.setData(node1.getData());
+        node1.setData(temp);
+    }
+
+    public void bubbleSort() {
+        for (int i = 0; i < size; i++) {
+            Node curNode = head;
+            for (int j = 0; j < size - i - 1; j++) {
+                if (curNode.getNext() != null) {
+                    if (curNode.getData() > curNode.getNext().getData()) {
+                        swap(curNode, curNode.getNext());
+                    }
+                }
+                curNode = curNode.getNext();
+            }
+        }
+    }
+
+    public void linearInputList(int n) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+            if (size == 0) {
+                add(value);
+            } else {
+                Node curNode = head;
+                Node prevNode = null;
+                while (curNode.getNext() != null && value > curNode.getData()) {
+                    prevNode = curNode;
+                    curNode = curNode.getNext();
+                }
+                if (curNode.getNext() == null && value > curNode.getData()) {
+                    curNode.setNext(newNode);
+                    tail = newNode;
+                }
+                else if (prevNode == null) {
+                    newNode.setNext(head);
+                    head = newNode;
+                } else if (value > curNode.getData()) {
+                    curNode.setNext(newNode);
+                    tail = newNode;
+                } else {
+                    prevNode.setNext(newNode);
+                    newNode.setNext(curNode);
+                }
+                size++;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String str;
+        Node temp = head;
+        StringBuilder strBuilder = new StringBuilder();
+        while (temp.getNext() != null) {
+            strBuilder.append(temp.getData()).append(" ");
+            temp = temp.getNext();
+        }
+        strBuilder.append(temp.getData());
+        str = strBuilder.toString();
+        return str;
+    }
+
+
+
+    // Следующие методы статик потому что они не нужны классу MyLinkedList,
+    // методы восприняты как абстрактные таски - просто на работу с ЛинкедЛистом
+    // но выносить в отдельный класс не захотелось :/
+    public static boolean delete2First(MyLinkedList list) {
+        if (list.size < 3) {
+            return false;
+        }
+        Node curNode = list.head;
+        for (int i = 0; i < 2; i++) {
+            curNode = curNode.getNext();
+        }
+        list.head = curNode;
+        list.size -= 2;
+        return true;
+    }
+
+    public static boolean removeValue(int value, MyLinkedList list) {
+        if (list.head == null) {
+            return false;
+        }
+
+        Node prevNode = null;
+        Node curNode = list.head;
+
+        if (curNode.getData() == value) {
+            if (curNode == list.tail) {
+                list.head = null;
+            } else {
+                list.head = curNode.getNext();
+            }
+            list.size--;
+            return true;
+        }
+
+        while (curNode != null) {
+            if (curNode.getData() == value) {
+                if (curNode == list.tail) {
+                    prevNode.setNext(null);
+                    list.tail = prevNode;
+                } else {
+                    prevNode.setNext(curNode.getNext());
+                }
+                list.size--;
+                return true;
+            }
+            prevNode = curNode;
+            curNode = curNode.getNext();
+        }
+        return false;
+    }
+
+    public static boolean removeAllValues(int value, MyLinkedList list) {
+        Node curNode = list.head;
+        Node prevNode = null;
+        boolean deleted = false;
+
+        if (curNode.getData() == value) {
+            if (curNode == list.tail) {
+                list.head = null;
+                list.size--;
+                return true;
+            }
+        }
+
+        while (curNode != null) {
+            if (curNode.getData() == value) {
+                if (prevNode == null) {
+                    list.head = curNode.getNext();
+                } else if (curNode == list.tail) {
+                    prevNode.setNext(null);
+                    list.tail = prevNode;
+                } else {
+                    prevNode.setNext(curNode.getNext());
+                }
+                list.size--;
+                deleted = true;
+            } else {
+                prevNode = curNode;
+            }
+            curNode = curNode.getNext();
+        }
+        return deleted;
+    }
+
+    public static void insertYafterX(int x, int y, MyLinkedList list) {
+        Node curNode = list.head;
+        Node prevNode = null;
+
+        while (curNode != null) {
+            if (curNode.getData() == x) {
+                Node newNode = new Node(y);
+                if (curNode == list.tail) {
+                    curNode.setNext(newNode);
+                    list.tail = newNode;
+                } else {
+                    newNode.setNext(curNode.getNext());
+                    curNode.setNext(newNode);
+                }
+                list.size++;
+                return;
+            }
+            prevNode = curNode;
+            curNode = curNode.getNext();
+        }
+    }
+
+    public static void insertYbeforeX(int x, int y, MyLinkedList list) {
+        Node curNode = list.head;
+        Node prevNode = null;
+
+        while (curNode != null) {
+            if (curNode.getData() == x) {
+                Node newNode = new Node(y);
+                if (prevNode == null) {
+                    newNode.setNext(list.head);
+                    list.head = newNode;
+                } else {
+                    newNode.setNext(curNode);
+                    prevNode.setNext(newNode);
+                }
+                list.size++;
+                return;
+            }
+            prevNode = curNode;
+            curNode = curNode.getNext();
+        }
+    }
+
+    public static void insertYafterAllX(int x, int y, MyLinkedList list) {
+        Node curNode = list.head;
+        Node prevNode = null;
+
+        while (curNode != null) {
+            if (curNode.getData() == x) {
+                Node newNode = new Node(y);
+                if (curNode == list.tail) {
+                    curNode.setNext(newNode);
+                    list.tail = newNode;
+                } else {
+                    newNode.setNext(curNode.getNext());
+                    curNode.setNext(newNode);
+                }
+                list.size++;
+            }
+            prevNode = curNode;
+            curNode = curNode.getNext();
+        }
+    }
+
+}
---
+++ 1c2s/homework/hw1/Node.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class Node {
+
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+        next = null;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+}
