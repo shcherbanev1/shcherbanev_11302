228c0005a9dcee05e20b06c8d5dfb588bbf254e7
shcherbanev1 <shcherbanev1@yandex.ru>
2024-05-10 18:44:21 +0300

    rename Main

--- 1c2s/homework/hw4/src/task1/Main1.java
+++ 1c2s/homework/hw4/src/task1/Main1.java
@@ -3,7 +3,7 @@ package task1;
 import java.io.File;
 import java.io.IOException;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) throws IOException {
         Converter converter = new UTF8ToWin1251Converter(new File("resources/utf8.txt"), new File("resources/win1251.txt"));
         converter.convert();
