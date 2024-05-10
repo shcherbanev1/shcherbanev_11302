48408375895d54689584657f6bd8f2cc77c47a3c
shcherbanev1 <shcherbanev1@yandex.ru>
2024-05-10 18:49:54 +0300

    use try-with-resources

--- 1c2s/homework/hw4/src/task1/UTF8ToWin1251Converter.java
+++ 1c2s/homework/hw4/src/task1/UTF8ToWin1251Converter.java
@@ -10,37 +10,36 @@ public class UTF8ToWin1251Converter extends Converter {
 
     @Override
     public void convert() throws IOException {
-        InputStream inputStream = new FileInputStream(input);
-        OutputStream outputStream = new FileOutputStream(output);
-        int first = inputStream.read();
-        int second;
-        while (first != -1) {
-            if (first == 208) {
-                second = inputStream.read();
-                if (second == 129) {
-                    outputStream.write(168);
-                }
-                else if (second >= 144 && second <= 191) outputStream.write(second + 48);
-                else {
+        try (InputStream inputStream = new FileInputStream(input);
+        OutputStream outputStream = new FileOutputStream(output)) {
+            int first = inputStream.read();
+            int second;
+            while (first != -1) {
+                if (first == 208) {
+                    second = inputStream.read();
+                    if (second == 129) {
+                        outputStream.write(168);
+                    }
+                    else if (second >= 144 && second <= 191) outputStream.write(second + 48);
+                    else {
+                        outputStream.write(first);
+                        outputStream.write(second);
+                    }
+                } else if (first == 209) {
+                    second = inputStream.read();
+                    if (second == 145) {
+                        outputStream.write(184);
+                    }
+                    else if (second >= 128 && second <= 143) outputStream.write(second + 112);
+                    else {
+                        outputStream.write(first);
+                        outputStream.write(second);
+                    }
+                } else {
                     outputStream.write(first);
-                    outputStream.write(second);
                 }
-            } else if (first == 209) {
-                second = inputStream.read();
-                if (second == 145) {
-                    outputStream.write(184);
-                }
-                else if (second >= 128 && second <= 143) outputStream.write(second + 112);
-                else {
-                    outputStream.write(first);
-                    outputStream.write(second);
-                }
-            } else {
-                outputStream.write(first);
+                first = inputStream.read();
             }
-            first = inputStream.read();
         }
-        inputStream.close();
-        outputStream.close();
     }
 }
