63626b48c7802da06a73ffe156a19d6317eda4a6
shcherbanev1 <shcherbanev1@yandex.ru>
2024-05-28 22:59:40 +0300

    hot fix

--- 1c2s/homework/hw4/src/task1/Main1.java
+++ 1c2s/homework/hw4/src/task1/Main1.java
@@ -4,8 +4,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Converter converter = new UTF8ToWin1251Converter(new File("resources/utf8.txt"), new File("resources/win1251.txt"));
-        converter.convert();
+        try {
+            converter.convert();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
